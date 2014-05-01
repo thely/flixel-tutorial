c1b18e76bb9d677a3102c6a75f1b8b21f5895519
SeiferTim <seifertim@gmail.com>
2014-05-01 10:54:05 -0500

    Finished Part III

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -5,19 +5,31 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
+import flixel.util.FlxAngle;
+import flixel.util.FlxDestroyUtil;
 import flixel.util.FlxMath;
+import flixel.util.FlxPoint;
 
 /**
  * A FlxState which can be used for the actual gameplay.
  */
 class PlayState extends FlxState
 {
+	
+	private var _player:Player;
+	
 	/**
 	 * Function that is called up when to state is created to set it up. 
 	 */
 	override public function create():Void
 	{
+		
+		_player = new Player(20, 20);
+		add(_player);
+		
 		super.create();
+		
+		
 	}
 	
 	/**
@@ -29,11 +41,78 @@ class PlayState extends FlxState
 		super.destroy();
 	}
 
+	
+	private function playerMovement():Void
+	{
+		
+		var _up:Bool = false;
+		var _down:Bool = false;
+		var _left:Bool = false;
+		var _right:Bool = false;
+		
+		#if !FLX_NO_KEYBOARD
+		_up = FlxG.keys.anyPressed(["UP", "W"]);
+		_down = FlxG.keys.anyPressed(["DOWN", "S"]);
+		_left = FlxG.keys.anyPressed(["LEFT", "A"]);
+		_right = FlxG.keys.anyPressed(["RIGHT", "D"]);
+		#end
+		
+		if (_up && _down)
+			_up = _down = false;
+		if (_left && _right)
+			_left = _right = false;
+		
+		if ( _up || _down || _left || _right)
+		{
+
+			var mA:Float = 0;
+			if (_up)
+			{
+				mA = -90;
+				if (_left)
+					mA -= 45;
+				else if (_right)
+					mA += 45;
+			}
+			else if (_down)
+			{
+				mA = 90;
+				if (_left)
+					mA += 45;
+				else if (_right)
+					mA -= 45;
+			}
+			else if (_left)
+				mA = 180;
+			else if (_right)
+				mA = 0;
+				
+			var v:FlxPoint = FlxAngle.rotatePoint(_player.Speed, 0, 0, 0, mA);
+			_player.velocity.x = v.x;
+			_player.velocity.y = v.y;
+			v = FlxDestroyUtil.put(v);
+		}
+		
+		if (!(_up || _down))
+			if (Math.abs(_player.velocity.y) >= _player.Friction)
+				_player.velocity.y *= _player.Friction;
+			else
+				_player.velocity.y = 0;
+		if (!(_left || _right))
+			if (Math.abs(_player.velocity.x) >= _player.Friction)
+				_player.velocity.x *= _player.Friction;
+			else
+				_player.velocity.x = 0;
+		
+	}
+	
 	/**
 	 * Function that is called once every frame.
 	 */
 	override public function update():Void
 	{
+		playerMovement();
+		
 		super.update();
 	}	
 }
--- source/Player.hx
+++ source/Player.hx
@@ -1,14 +1,18 @@
 package ;
 
 import flixel.FlxSprite;
+import flixel.util.FlxColor;
 
 class Player extends FlxSprite
 {
 
-	public function new(X:Float=0, Y:Float=0, ?SimpleGraphic:Dynamic) 
+	public var Speed:Float = 200;
+	public var Friction:Float = .6;
+	
+	public function new(X:Float=0, Y:Float=0) 
 	{
-		super(X, Y, ?SimpleGraphic);
-		
+		super(X, Y);
+		makeGraphic(16, 16, FlxColor.BLUE);
 	}
 	
 }
