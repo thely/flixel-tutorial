3773ecf32d3a1d7fe4d20b39bfbb10efefd64579
Tim I Hely <seifertim@gmail.com>
2014-05-07 10:13:35 -0500

    Update Player.hx

--- source/Player.hx
+++ source/Player.hx
@@ -2,6 +2,10 @@ package ;
 
 import flixel.FlxSprite;
 import flixel.util.FlxColor;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.util.FlxAngle;
+import flixel.FlxG;
 
 class Player extends FlxSprite
 {
@@ -14,6 +18,59 @@ class Player extends FlxSprite
 		makeGraphic(16, 16, FlxColor.BLUE);
 		drag.x = drag.y = 1600;
 		
+	}
+	
+	private function movement():Void
+	{
+		
+		var _up:Bool = false;
+		var _down:Bool = false;
+		var _left:Bool = false;
+		var _right:Bool = false;
+		
+		_up = FlxG.keys.anyPressed(["UP", "W"]);
+		_down = FlxG.keys.anyPressed(["DOWN", "S"]);
+		_left = FlxG.keys.anyPressed(["LEFT", "A"]);
+		_right = FlxG.keys.anyPressed(["RIGHT", "D"]);
+		
+		if (_up && _down)
+			_up = _down = false;
+		if (_left && _right)
+			_left = _right = false;
+		
+		if ( _up || _down || _left || _right)
+		{
+
+			var mA:Float = 0;
+			if (_up)
+			{
+				mA = -90;
+				if (_left)
+					mA -= 45;
+				else if (_right)
+					mA += 45;
+			}
+			else if (_down)
+			{
+				mA = 90;
+				if (_left)
+					mA += 45;
+				else if (_right)
+					mA -= 45;
+			}
+			else if (_left)
+				mA = 180;
+			else if (_right)
+				mA = 0;
+				
+			FlxAngle.rotatePoint(_player.Speed, 0, 0, 0, mA, _player.velocity);
+		}		
+	}
+	
+	override public function update():Void 
+	{
+		movement();
+		super.update();
 	}
 	
 }
