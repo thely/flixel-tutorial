e76172d24a0c0994f0b60f029c56ce6b5637fe2b
Tim I Hely <seifertim@gmail.com>
2014-05-07 10:15:18 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -40,69 +40,12 @@ class PlayState extends FlxState
 	}
 
 	
-	private function userInput():Void
-	{
-		
-		var _up:Bool = false;
-		var _down:Bool = false;
-		var _left:Bool = false;
-		var _right:Bool = false;
-		
-		_up = FlxG.keys.anyPressed(["UP", "W"]);
-		_down = FlxG.keys.anyPressed(["DOWN", "S"]);
-		_left = FlxG.keys.anyPressed(["LEFT", "A"]);
-		_right = FlxG.keys.anyPressed(["RIGHT", "D"]);
-		
-		if (_up && _down)
-			_up = _down = false;
-		if (_left && _right)
-			_left = _right = false;
-		
-		if ( _up || _down || _left || _right)
-		{
-
-			var mA:Float = 0;
-			if (_up)
-			{
-				mA = -90;
-				if (_left)
-					mA -= 45;
-				else if (_right)
-					mA += 45;
-					
-				_player.facing = FlxObject.UP;
-			}
-			else if (_down)
-			{
-				mA = 90;
-				if (_left)
-					mA += 45;
-				else if (_right)
-					mA -= 45;
-				
-				_player.facing = FlxObject.DOWN;
-			}
-			else if (_left)
-			{
-				mA = 180;
-				_player.facing = FlxObject.LEFT;
-			}
-			else if (_right)
-			{
-				mA = 0;
-				_player.facing = FlxObject.RIGHT;
-			}
-			FlxAngle.rotatePoint(_player.speed, 0, 0, 0, mA, _player.velocity);
-		}
-	}
-	
 	/**
 	 * Function that is called once every frame.
 	 */
 	override public function update():Void
 	{
-		userInput();
-		
+
 		super.update();
 	}	
 }
