95ff59cfb1c2a9e30a7d5a4294af1d8206aeb04e
SeiferTim <seifertim@gmail.com>
2014-05-13 14:48:24 -0500

    Working on Part X

--- source/Main.hx
+++ source/Main.hx
@@ -5,8 +5,10 @@ import flash.display.StageAlign;
 import flash.display.StageScaleMode;
 import flash.events.Event;
 import flash.Lib;
+import flixel.FlxG;
 import flixel.FlxGame;
 import flixel.FlxState;
+import flixel.util.FlxSave;
 
 class Main extends Sprite 
 {
@@ -61,8 +63,16 @@ class Main extends Sprite
 			zoom = Math.min(ratioX, ratioY);
 			gameWidth = Math.ceil(stageWidth / zoom);
 			gameHeight = Math.ceil(stageHeight / zoom);
-		}
+		}		
 
 		addChild(new FlxGame(gameWidth, gameHeight, initialState, zoom, framerate, framerate, skipSplash, startFullscreen));
+		
+		var _save:FlxSave = new FlxSave();
+		_save.bind("flixel-tutorial");
+		if (_save.data.volume != null)
+		{
+			FlxG.sound.volume = _save.data.volume;
+		}
+		_save.close();
 	}
 }
--- source/OptionsState.hx
+++ source/OptionsState.hx
@@ -6,6 +6,7 @@ import flixel.text.FlxText;
 import flixel.ui.FlxBar;
 import flixel.ui.FlxButton;
 import flixel.util.FlxColor;
+import flixel.util.FlxSave;
 using flixel.util.FlxSpriteUtil;
 
 class OptionsState extends FlxState
@@ -21,6 +22,8 @@ class OptionsState extends FlxState
 	private var _btnClearData:FlxButton;
 	private var _btnBack:FlxButton;
 	
+	private var _save:FlxSave;
+	
 	override public function create():Void 
 	{
 		
@@ -34,27 +37,72 @@ class OptionsState extends FlxState
 		_txtVolume.screenCenter(true, false);
 		add(_txtVolume);
 		
-		_btnVolumeDown = new FlxButton(10, _txtVolume.y + _txtVolume.height + 8, "-", clickVolumeDown);
+		_btnVolumeDown = new FlxButton(8, _txtVolume.y + _txtVolume.height + 2, "-", clickVolumeDown);
+		_btnVolumeDown.loadGraphic(AssetPaths.button__png, true, 20,20);
 		add(_btnVolumeDown);
-		_btnVolumeUp = new FlxButton(0, _btnVolumeDown.y, "+", clickVolumeUp);
-		_btnVolumeUp.x = FlxG.width - _btnVolumeUp.width - 8;
+		
+		_btnVolumeUp = new FlxButton(FlxG.width - 28, _btnVolumeDown.y, "+", clickVolumeUp);
+		_btnVolumeUp.loadGraphic(AssetPaths.button__png, true, 20,20);
 		add(_btnVolumeUp);
 		
-		_barVolume = new FlxBar(_btnVolumeDown.x + _btnVolumeDown.width + 2, _btnVolumeDown.y, FlxBar.FILL_LEFT_TO_RIGHT, Std.int(FlxG.width - _btnVolumeDown.width - _btnVolumeUp.width - 20), Std.int(_btnVolumeUp.height));
+		_barVolume = new FlxBar(_btnVolumeDown.x + _btnVolumeDown.width + 4, _btnVolumeDown.y, FlxBar.FILL_LEFT_TO_RIGHT, Std.int(FlxG.width - 64), Std.int(_btnVolumeUp.height));
 		_barVolume.createFilledBar(FlxColor.CHARCOAL, FlxColor.WHITE, true, FlxColor.WHITE);
 		add(_barVolume);
 		
+		_txtVolumeAmt = new FlxText(0, 0, 200, Std.string( FlxG.sound.volume * 100) + "%", 8);
+		_txtVolumeAmt.alignment = "center";
+		_txtVolumeAmt.borderStyle = FlxText.BORDER_OUTLINE;
+		_txtVolumeAmt.borderColor = FlxColor.CHARCOAL;
+		_txtVolumeAmt.y = _barVolume.y + (_barVolume.height / 2) - (_txtVolumeAmt.height / 2);
+		_txtVolumeAmt.screenCenter(true, false);
+		add(_txtVolumeAmt);
+		
+		_btnClearData = new FlxButton((FlxG.width / 2) - 90, FlxG.height - 28, "Clear Data", clickClearData);
+		add(_btnClearData);
+		
+		_btnBack = new FlxButton((FlxG.width/2)+10, FlxG.height-28, "Back", clickBack);
+		add(_btnBack);
+		
+		
+		_save = new FlxSave();
+		_save.bind("flixel-tutorial");
+		
+		updateVolume();
 		
 		super.create();
 	}
 	
+	private function clickClearData():Void
+	{
+		_save.erase();
+		FlxG.sound.volume = .5;
+		updateVolume();
+	}
+	
+	private function clickBack():Void
+	{
+		_save.close();
+		FlxG.switchState(new MenuState());
+	}
+	
 	private function clickVolumeDown():Void
 	{
-		
+		FlxG.sound.volume -= 0.1;
+		updateVolume();
 	}
 	
 	private function clickVolumeUp():Void
 	{
+		FlxG.sound.volume += 0.1;
+		updateVolume();
+	}
+	
+	private function updateVolume():Void
+	{
+		_save.data.volume = FlxG.sound.volume;
+		var vol:Int = Math.round(FlxG.sound.volume * 100);
+		_barVolume.currentValue = vol;
+		_txtVolumeAmt.text = Std.string(vol) + "%";
 		
 	}
 }
