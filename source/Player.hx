f3928f88d2b5fcfcc4b686ee8a3c8e45167cbfcd
SeiferTim <seifertim@gmail.com>
2014-05-29 09:19:23 -0500

    Added Vicky's Player Sprite

--- source/Player.hx
+++ source/Player.hx
@@ -17,9 +17,9 @@ class Player extends FlxSprite
 		loadGraphic("assets/images/player.png", true, 16, 16);
 		setFacingFlip(FlxObject.LEFT, false, false);
 		setFacingFlip(FlxObject.RIGHT, true, false);
-		animation.add("lr", [1, 0], 6, false);
-		animation.add("u", [3, 2], 6, false);
-		animation.add("d", [5, 4], 6, false);
+		animation.add("d", [0, 1, 0, 2], 6, false);
+		animation.add("lr", [3, 4, 3, 5], 6, false);
+		animation.add("u", [6, 7, 6, 8], 6, false);
 		drag.x = drag.y = 1600;
 		width = 8;
 		height = 14;
